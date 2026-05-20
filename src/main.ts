90b94c64520b4be7368fb5149deeca41df9664ea
Tanoh ezekiel <tanohezekiel32@gmail.com>
2026-05-20 15:05:10 +0000

    Update CORS

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-
-  await app.listen(4000);
-}
-bootstrap();
+app.enableCors({
+  origin: [
+    'http://localhost:3000',
+    'https://https://bizora-frontend-ivory.vercel.app/',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
