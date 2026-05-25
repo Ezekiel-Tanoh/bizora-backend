7ef0d06741f22c402c12dbe5fcc5a8de7bd2ee14
Tanoh ezekiel <tanohezekiel32@gmail.com>
2026-05-25 07:54:53 +0000

    Fix payments files

--- src/payments/payments.controller.spec.ts
+++ src/payments/payments.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentsController } from './payments.controller';
+import { describe, beforeEach, it, expect } from '@jest/globals';
 
 describe('PaymentsController', () => {
   let controller: PaymentsController;
